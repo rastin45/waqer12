49347e3e6a95400d9eab37005e4f255a55dfa91b
rastin45 <miladworld514@gmail.com>
2015-12-20 09:04:59 +0000

    Update groupmanager.lua

--- plugins/groupmanager.lua
+++ plugins/groupmanager.lua
@@ -5,7 +5,7 @@ do
 local function create_group(msg)
     -- superuser and admins only (because sudo are always has privilege)
     if not is_admin(msg) then
-        return "This service is private for SUDO (@raswtinblowndi)"
+        return "This service is private for SUDO (@rastinblowndi)"
     end
     local group_creator = msg.from.print_name
     create_group_chat (group_creator, group_name, ok_cb, false)
