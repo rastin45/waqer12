8b323b9300e5f05c2b86dd93ba36eb5ec4fcf318
rastin45 <miladworld514@gmail.com>
2015-12-20 14:19:41 +0000

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -2,7 +2,7 @@ do
 
 function run(msg, matches)
   return 'Telegram Bot '.. VERSION .. [[ 
-no version _no fuzuli _bilakh sicktir .com  @rastin44 for more info.]]
+no version _no fuzuli _bilakh sicktir .com  @rastinblowndi&@nfrat_shah. for more info.]]
 end
 
 return {
